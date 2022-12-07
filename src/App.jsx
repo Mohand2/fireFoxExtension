a6a79c7aef3a94ae7551a10ec27ffe7096b4dd32
moh <alloun.mohand@gmail.com>
2022-12-07 23:52:25 +0100

    weather app extension v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import './app.css';
 
 function App() {
   const [weather, setweather] = useState([]);
+  const [location] = useState({
+    lat: 36.76,
+    long: 3.15,
+  });
+  // const [city, setcity] = useState('paris');
+  const [isLoading, setisLoading] = useState(true);
 
+  // fetch api
   useEffect(() => {
-    const API_URL =
-      'https://api.open-meteo.com/v1/forecast?latitude=36.76&longitude=3.15&daily=temperature_2m_min&timezone=Europe%2FLondon';
+    const API_URL = `https://api.open-meteo.com/v1/forecast?latitude=${location.lat}&longitude=${location.long}&daily=temperature_2m_min&timezone=Europe%2FLondon`;
 
     async function fetchWeather() {
+      setisLoading(true);
       const response = await fetch(API_URL);
       const data = await response.json();
 
@@ -18,13 +25,19 @@ function App() {
         data.daily_units.temperature_2m_min
       );
       setweather(daysAndT);
+      setisLoading(false);
     }
     fetchWeather();
   }, []);
 
   function getDaysAndTemp(days, temp, unit) {
     return days.map((date, index) => {
-      return { day: getDay(date), date: date, temp: temp[index], unit: unit };
+      return {
+        day: getDay(date),
+        date: date,
+        temp: Math.round(temp[index]),
+        unit: unit,
+      };
     });
   }
 
@@ -48,14 +61,17 @@ function App() {
 
   return (
     <div className="weather">
-      <h1>Mchedallah</h1>
-      {weather.length
+      <h2>Mchedallah</h2>
+      {!isLoading
         ? weather.map((day, index) => (
             <div className="weather-card" key={index}>
-              <p>{day.day}</p>
-              <p>{day.date}</p>
+              <div className="weather-leading">
+                <p>{day.day}</p>
+                <p className="date">{day.date}</p>
+              </div>
               <p>
-                {day.temp} <spna>{day.unit}</spna>{' '}
+                <span>{day.temp}</span>
+                {day.unit}
               </p>
             </div>
           ))
