97edce064c9f9b59d742b94296ae46b7e6efbcf7
moh <alloun.mohand@gmail.com>
2022-12-07 21:03:56 +0100

    add getDays andTem function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './app.css';
 
 function App() {
-  return <div className="App">my react app </div>;
+  const [weather, setweather] = useState([]);
+
+  useEffect(() => {
+    const API_URL =
+      'https://api.open-meteo.com/v1/forecast?latitude=36.76&longitude=3.15&daily=temperature_2m_min&timezone=Europe%2FLondon';
+
+    async function fetchWeather() {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+
+      const daysAndT = getDaysAndTemp(
+        data.daily.time,
+        data.daily.temperature_2m_min
+      );
+      console.log(daysAndT[0].temp);
+    }
+    fetchWeather();
+  }, []);
+
+  function getDaysAndTemp(days, temp) {
+    return days.map((date, index) => {
+      return { date: date, temp: temp[index] };
+    });
+  }
+
+  const loading = <div>Loading...</div>;
+
+  console.log(weather.daily);
+
+  return <p>test</p>;
+
+  // <div className="App">{weather ? weather.daily : loading}</div>;
 }
 
 export default App;
